89ef8c97c903a54ed3474f6368c62d0556e931ee
skwinnik <screen0994@gmail.com>
2023-01-18 18:40:31 +0000

    fix: feels like rounding

--- components/feels-like/feels-like.tsx
+++ components/feels-like/feels-like.tsx
@@ -1,7 +1,7 @@
 export default function FeelsLike({ temp }: { temp: number }) {
   return (
     <div className="flex flex-col justify-between h-full">
-      <div className="text-3xl">{temp}°</div>
+      <div className="text-3xl">{temp.toFixed(0)}°</div>
       <div className="text-sm">Lorem Ipsum</div>
     </div>
   );
