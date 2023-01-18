60280e7f580240bfae23bd169e1779d5ce195d9d
skwinnik <screen0994@gmail.com>
2023-01-18 16:34:01 +0000

    fix: improve headers

--- components/tile/tile.tsx
+++ components/tile/tile.tsx
@@ -15,7 +15,9 @@ export default function Tile({
     >
       <div className="text-primary-300 text-sm [&>svg]:h-4 [&>svg]:w-4 [&>svg]:inline-block [&>svg]:align-sub flex-auto flex-grow-0 flex-shrink-0">
         {icon}&nbsp;
-        <span>{header}</span>
+        <span className="uppercase font-bold tracking-wider text-xs">
+          {header}
+        </span>
       </div>
       <div
         className="border-t border-primary-500 mt-2 pt-2 flex-1"
