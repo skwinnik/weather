3caa69abce4d7749b4515b691ebd978cb32dfc66
skwinnik <screen0994@gmail.com>
2023-01-20 10:09:55 +0000

    fix: set meta theme

--- app/head.tsx
+++ app/head.tsx
@@ -1,8 +1,10 @@
 export default function Head() {
+  const primaryColor = "#0284c7";
   return (
     <>
-      <title>Create Next App</title>
+      <title>Weather</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="theme-color" content={primaryColor}></meta>
       <link rel="icon" href="/favicon.ico" />
     </>
   );
